b2cf75ee36ae99e5c3a54c05c0b9027bcba98dbc
soufiane-ekouines <soufianeekouines@gmail.com>
2022-10-01 11:14:54 +0100

    change the itle

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -8,10 +8,17 @@ import '../styles/Header.css';
 
 const Header = () => {
     const [value, setValue] = React.useState(dayjs('2022-04-07'));
-    // console.log(value);
+    const [hour,sethour]=React.useState(value.$H);
+    const [m,setm]=React.useState(value.$m);
+
+    for(const i=0;i<8;i++)
+    {
+      sethour(hour+i);
+    }
+
     return (
       <div className='content'>
-        <h1 className='Infotitle'>Sleep Calculator {value.$H} {value.$m} </h1>
+        <h1 className='Infotitle'>Sleep Calculator </h1>
         <h1 className='Info'>What time do you want to wake up?</h1>
         <div className='time'>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -28,7 +35,8 @@ const Header = () => {
             </LocalizationProvider>
         </div>
         <h1 className='Info'>If you want to go bed Now..</h1>
-        <button className='Calcul'>Wake-up time {value.$H+15}:{value.$m}</button>
+        
+        <button className='Calcul'>Wake-up time {hour} </button>
 
       </div>
     );
